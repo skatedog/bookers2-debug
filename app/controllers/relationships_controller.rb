637fb67428832465204e73a18e052e5f862cbc08
skatedog <skatedog0001@gmail.com>
2021-09-06 12:35:26 +0000

    [Fix]リファクタリング

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,16 +1,18 @@
 class RelationshipsController < ApplicationController
   def create
-    relationship = Relationship.new
-    relationship.follower_id = current_user.id
-    relationship.followed_id = params[:id]
-    relationship.save
+    current_user.followeds_list.create(followed_id: params[:id])
+    # relationship = Relationship.new
+    # relationship.follower_id = current_user.id
+    # relationship.followed_id = params[:id]
+    # relationship.save
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    relationships = Relationship.all
-    relationship = relationships.where(follower_id: current_user.id, followed_id: params[:id])
-    relationship.destroy_all
+    current_user.followeds_list.find_by(followed_id: params[:id]).destroy
+    # relationships = Relationship.all
+    # relationship = relationships.where(follower_id: current_user.id, followed_id: params[:id])
+    # relationship.destroy_all
     redirect_back(fallback_location: root_path)
   end
 end
