10e589e5975ddf07f971180ffef42c3912a36c18
skatedog <skatedog0001@gmail.com>
2021-09-09 06:41:37 +0000

    [Finish]非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    @new_book = Book.new
-    @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = @book.id
-    if @book_comment.save
-      redirect_to book_path(@book.id)
-    else
-      render "books/show"
-    end
+    book_comment = current_user.book_comments.new(book_comment_params)
+    book_comment.book_id = @book.id
+    book_comment.save
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     comment = BookComment.find(params[:id])
     if comment.user == current_user
       comment.destroy
-      redirect_to book_path(params[:book_id])
     else
       redirect_to book_path(params[:book_id])
     end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find_by(id: params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find_by(id: params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find_by(id: params[:book_id])
-    favorite = book.favorites.find_by(user_id: current_user.id)
+    @book = Book.find_by(id: params[:book_id])
+    favorite = @book.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
