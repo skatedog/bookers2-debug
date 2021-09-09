d761e5c6a4506622f278f485ebc3ed9058aba02e
skatedog <skatedog0001@gmail.com>
2021-09-09 08:06:01 +0000

    [Fix]コメント数の更新機能追加,エラーメッセージ表示

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,11 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = @book.id
-    book_comment.save
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    unless @book_comment.save
+      render "error"
+    end
   end
 
   def destroy
